9a3ec88f2c270173d84d42dabd7ccf3fad962366
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2021-10-11 09:12:24 +0200

    bBasic template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
-<h1>home</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam amet, architecto assumenda at atque autem
-    beatae earum excepturi expedita explicabo hic illo nesciunt numquam, optio qui quibusdam sequi sint tempora unde
-    velit veniam, vitae voluptate. Cumque in nobis quisquam.</p>
+@extends('layouts.template')
+
+@section('title', 'Welcome to the vinyl shop')
+
+@section('main')
+<h1>The vinyl shop</h1>
+<p>Welcome ...</p>
+@endsection
---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="container border-top">
+    <div class="row justify-content-end"><p class="order-1">Footer</p></div>
+</footer>
