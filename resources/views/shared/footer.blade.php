5aef035e33e377315f81134cbed4fd0361f25fcf
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2021-10-18 09:27:24 +0200

    mysql

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="container text-right border-top text-primary">
+<footer class="container text-right border-top text-muted">
    <p class="mr-5">The Vinyl Shop - &#169; {{date("Y")}}
     <br>
-    <span class="text-muted">Laravel v: {{ app()->version() }}</span></p>
+    <span>Laravel v: {{ app()->version() }}</span></p>
 </footer>
