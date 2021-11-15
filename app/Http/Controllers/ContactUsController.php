938bd0beec2f893a8888ece1d9bd27bf65bb41e1
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2021-11-15 10:36:11 +0100

    update

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Mail;
 
 class ContactUsController extends Controller
 {
@@ -12,19 +14,29 @@ class ContactUsController extends Controller
         return view('contact');
     }
 
+
     // Send email
     public function sendEmail(Request $request)
     {
         // Flash a success message to the session
-
+        /*$request->flash();*/
         // Validate form
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required|min:10'
+        ]);
 
         // Send email
-
+        $email = new ContactMail();
+        // return $email;       // uncomment this line to display the result in the browser
+        Mail::to($request)      // or Mail::to($request->email, $request->name)
+        ->send($email);
         // Flash filled-in form values to the session
+        session()->flash('success', 'Thank you for your message.<br>We\'ll contact you as soon as possible.');
 
         // Redirect to the contact-us link ( NOT to view('contact')!!! )
-        return $request;
+
         return redirect('contact-us');
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,32 +3,41 @@
 @section('title', 'Contact us')
 @section('main')
     <h1>Contact us</h1>
-    <form action="/contact-us" method="post">
-        @csrf // cross-site request forgery bij elke form
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name"
-                   class="form-control"
-                   placeholder="Your name"
-                   required
-                   value="">
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email"
-                   class="form-control"
-                   placeholder="Your email"
-                   required
-                   value="">
-        </div>
-        <div class="form-group">
-            <label for="message">Message</label>
-            <textarea name="message" id="message" rows="5"
-                      class="form-control"
-                      placeholder="Your message"
-                      required
-                      minlength="10"></textarea>
-        </div>
-        <button type="submit" class="btn btn-success">Send Message</button>
-    </form>
+
+    @include('shared.alert')
+
+    @if (!session()->has('success'))
+
+        <form action="/contact-us" method="post" novalidate>
+            @csrf {{--cross-site request forgery bij elke form--}}
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name"
+                       class="form-control {{ $errors->any() ? ($errors->first('name') ? 'is-invalid' : 'is-valid') : '' }}"
+                       placeholder="Your name"
+                       required
+                       value="{{ old('name') }}">
+                <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email"
+                       class="form-control {{ $errors->any() ? ($errors->first('email') ? 'is-invalid' : 'is-valid') : '' }}"
+                       placeholder="Your email"
+                       required
+                       value="{{ old('email') }}">
+                <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" rows="5"
+                          class="form-control {{ $errors->any() ? ($errors->first('message') ? 'is-invalid' : 'is-valid') : '' }}"
+                          required
+                          minlength="10">{{ old('message') }}</textarea>
+                <div class="invalid-feedback">{{ $errors->first('message') }}</div>
+            </div>
+            <button type="submit" class="btn btn-success">Send Message</button>
+        </form>
+
+    @endif
 @endsection
